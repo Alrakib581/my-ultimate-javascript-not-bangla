3e6854f8965094d96ae42683af7716454097275a
alrakib581 <alrakib581@gmail.com>
2022-08-05 22:39:26 +0600

    resreved keyword file added

---
+++ javaScript-reserve-keyword.js
@@ -0,0 +1,72 @@
+/**
+ * javascript এর কিছু নিজস্ব reserve keyword রয়েছে যেগুলো আমরা variable এর নাম হিসাবে ব্যাবহার  করতে পারবনা।
+ * variable এর নামের ক্ষেত্রে যদি একাধিক word ব্যাবহার করতে হয় ত word এর মাঝখানে কোন গ্যাপ দেয়া যাবেনা ।
+ * variable এর নামে কোন কোটেশন দেয়া যাবেনা। 
+ * variable এর নামের প্রথমে কোন সংখ্যা দেয়া যাবেনা। তবে শেসে দেয়া যাবে।
+ * variable এর নাম ক্যামেল কেসে লেখা উত্তম। তাই আমরা সব সময় ক্যামেল কেস ব্যাবহার করবো।
+ * কিছু reserved keyword নিচে দেওয়া হোলো।
+ * abstract
+ * arguments
+ * await*
+ * boolean 
+ * break
+ * byte	
+ * case	
+ * catch
+ * char
+ * class*	
+ * const	
+ * continue
+ * debugger
+ * default
+ * delete	
+ * do
+ * double
+ * else	
+ * enum*	
+ * eval
+ * export*	
+ * extends*	
+ * false	
+ * final
+ * finally	
+ * float	
+ * for	
+ * function
+ * goto	
+ * if	
+ * implements	
+ * import*
+ * in	
+ * instanceof	
+ * int	
+ * interface
+ * let*	
+ * long	
+ * native	
+ * new
+ * null	
+ * package	
+ * private	
+ * protected
+ * public	
+ * return	
+ * short	
+ * static
+ * super*	
+ * switch	
+ * synchronized	
+ * this
+ * throw	
+ * throws	
+ * transient	
+ * true
+ * try	
+ * typeof	
+ * var	
+ * void
+ * volatile	
+ * while	
+ * with	
+ * yield
+ */
