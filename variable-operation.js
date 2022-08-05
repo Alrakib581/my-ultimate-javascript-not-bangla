fa23ca85187a116b812c391a1f64d9d90a946511
alrakib581 <alrakib581@gmail.com>
2022-08-05 23:26:34 +0600

    file added

---
+++ variable-operation.js
@@ -0,0 +1,29 @@
+var monitorPrice = 5900;
+var speakerPrice = 530;
+console.log(monitorPrice);// monitor variable এর মান দেখাবে।
+console.log(speakerPrice);// speaker variable এর মান দেখাবে।
+console.log(monitorPrice + speakerPrice); // আমরা চাইলে console.log(); এর মধ্যে ২ টা variable কে যোগ করতে পারি।
+var totalPrice = monitorPrice + speakerPrice; // আমরা চাইলে একটি নতুন variable declare করে তার মান হিসাবে অন্য variable গুলোকে যোগ, বিয়োগ, গুন, ভাগ  ও ভাগশেষ আমরা লিখতে ও তার ফল বের করতে পারি।
+console.log(totalPrice);
+
+var priceDifference = monitorPrice - speakerPrice; //বিয়োগ 
+console.log(priceDifference);
+
+// let eggPrice = 10;
+// let quantity = 30; গুণন 
+// let allEggPrice = 10 * 30;
+// console.log(allEggPrice);
+
+let eggPrice = 10;
+let quantity = 30;
+let allEggPrice = eggPrice * quantity;
+console.log(allEggPrice);
+
+const money = 5000;
+const kamla = 10;   // ভাগ
+const perkamlaMoney = money / kamla;
+console.log(perkamlaMoney);
+
+/**
+ * + , - , * , / , % এগুলোকে operator বলে আর যার উপর এগুলো অপারেশন চালায় তাদের operand বলে। 
+ */
