f268190657197c8b398241df49c4a7d8cde7e30b
alrakib581 <alrakib581@gmail.com>
2022-08-06 16:19:39 +0600

    comparison operator

---
+++ comparison.js
@@ -0,0 +1,58 @@
+/**
+ * == ২ টা মান সমান 
+ * === ২ টার মান এবং type সমান।
+ * != সমান না 
+ * !== ২ টার মান এবং type সমান নয় ।
+ * > greater than
+ * < less than
+ * >= বড় বা সমান
+ * <= ছোট বা সমান।
+ */
+
+
+/**
+ * 2 টা && মানে সব শর্ত সঠিক হতে হবে।
+ * 2 টা || মানে যেকোনো একটা শর্ত সঠিক হতে হবে।
+ */
+
+// console.log(5 > 8); // ৫,  ৮ এর থেকে বড় ? ans false
+
+// console.log(9 > 8); // ৯,  ৮ এর থেকে বড় ? ans true
+
+// console.log(9 < 10); // ১০ , ৯ এর থেকে ছোট ? ans true
+
+// console.log(11 < 10); // ১১, ১০ এর থেকে ছোট ? ans false
+
+//  console.log( 7 == 8 ); // ৭ আর ৮ কি সমান ? ans false
+
+//  console.log( 8 == 8 ); // ৮ আর ৮ কি সমান ? ans true
+
+
+//  console.log( 7 === 'rakib' ); // ৭ আর স্ট্রিং কি সমান ? ans false
+
+//  console.log( 'motin' === 'rakib' ); // ২ টা আলাদা স্ট্রিং কি সমান ? ans false
+
+//  console.log( 'rakib' === 'rakib' ); // ২ টা same স্ট্রিং কি সমান ? ans true
+
+// console.log(11 !== 10); // ১১, ১০ সমান নয় ? ans true
+
+// console.log(10 !== 10); // ১০, ১০ সমান নয় ? ans false
+
+// console.log(10 >= 10); // ১০, ১০ সমান বা বড় ? ans true
+
+// console.log(10 >= 12); // ১০, ১2 এর  সমান বা বড় ? ans false
+
+// console.log(13 >= 12); // ১৩, ১2 এর  সমান বা বড় ? ans true
+
+// console.log(10 <= 12); // ১০ কি ১২ এর ছোট  সমান ? ans false
+
+// console.log(15 <= 12); // ১৫ কি ১২ এর ছোট  সমান ? ans false     
+
+
+// let num1 = 10;
+// let num2 = 20; // variable comparison
+// console.log(num1 > num2);
+
+// let num1 = 10;
+// let num2 = 20; // variable comparison
+// console.log(num1 < num2);
