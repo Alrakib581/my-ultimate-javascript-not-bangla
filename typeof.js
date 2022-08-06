6ede54ae88a0bd68ad3bb7ccb7c49e88f00f9213
alrakib581 <alrakib581@gmail.com>
2022-08-06 10:12:25 +0600

    typeof file added

---
+++ typeof.js
@@ -0,0 +1,12 @@
+/**
+ * javascript এ data type বা কোনটা কোন ধরনের ডাটা সেটা চেক করার নিয়ম। 
+ */
+
+let rakibMoney = 100;        
+console.log(typeof rakibMoney);   // এটা দেখাবে নাম্বার ।
+
+let myName = 'Abder fattah al sisi';
+console.log(typeof myName);         // এটা দেখাবে স্ট্রিং।
+
+let isSingle = true;
+console.log(typeof isSingle); // এটা দেখাবে বুলিয়ান। 
