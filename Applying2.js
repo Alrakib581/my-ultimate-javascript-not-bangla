eaa1ad422c5d4f4538430400505c94d3006a0bec
alrakib581 <alrakib581@gmail.com>
2022-08-10 03:08:16 +0600

    file added

---
+++ Applying2.js
@@ -0,0 +1,30 @@
+// function getSum (numbers){
+//     let sum = 0;
+//     for ( let i = 0; i < numbers.length; i++){
+//         const index = i;
+//         const element = numbers[index];
+//         sum = sum + element;
+//         console.log(index,element,sum);
+//     }
+//     return sum;
+// }
+// const myNumbers = [12, 65, 45, 78, 32, 45, 91];
+// getSum(myNumbers)
+
+
+function getOddNumbersAnArray (numbers){
+  let sum = 0;
+    for ( let i = 0; i < numbers.length; i++){
+        const index = i;
+        const element = numbers[index];
+        
+        if(element % 2 !== 0){
+
+            console.log(index,element);
+        }
+        
+    }
+  
+}
+const myNumbers = [12, 65, 45, 78, 32, 45, 91];
+getOddNumbersAnArray(myNumbers)
---
+++ factorial.js
@@ -0,0 +1,41 @@
+// // let sum = 0;
+// // for(let i = 1; i <= 7; i++){
+// //     sum = sum + i
+// //     console.log(i,sum);
+// // }
+
+// function sumOfNumber(number) {
+//     let sum = 0;
+//     for (let i = 1; i <= number; i++) {
+//         sum = sum + i
+//         console.log(i, sum);
+//     }
+//     return sum;
+
+// }
+// sumOfNumber(1);
+
+// function mutip (number){
+//     let result = 1;
+//     for (let i = 1; i <= 7; i++){
+//         result = result *i;
+//     }
+//     return result;
+// }
+// const result = mutip(7);
+// console.log(result)
+
+
+//4 ! = 3*2*1
+//5 ! = 5*4*3*2*1
+
+// for (let i = 10; i >= 1; i-- ){
+//     console.log(i);
+// }
+
+function print ( a , b ,c){
+    return a+ b;
+
+
+}
+console.log(print(1,2,3));
