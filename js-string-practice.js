cfb054e76c32d8790da4e0b6ec0582cab678c604
alrakib581 <alrakib581@gmail.com>
2022-08-10 11:15:18 +0600

    file

---
+++ js-string-practice.js
@@ -0,0 +1,48 @@
+// const myAddress = '211 , shamoly tower .bangladesh';
+// console.log(myAddress.length)
+
+// const nam = 'ABM T';
+// console.log(nam.length);
+
+// const nam = 'ABM T';
+
+// console.log(nam[3]); //index number die upadan ke poa 
+
+// const userName = 'blackPink';
+// const userInput = 'blackPinK';
+// if (userName.toLowerCase() === userInput.toLowerCase() ){
+//     console.log('valid input');
+// }
+// else {
+//     console.log('gon willed');
+// }
+
+// const lirics = 'tumi bondhu kala pakhi ami jeno ki , boshonto kale tomay bolte parini. shada shada kala kala rong jomeche shada kala'
+// const liricsLowerCase = lirics.toLowerCase();
+// const searchString = 'Shada'
+// searchString.toLowerCase();
+// const existent = lirics.includes(liricsLowerCase);
+// console.log(existent);
+// console.log(lirics.indexOf('kala'));
+// if(lirics.indexOf('shada') !== -1){
+//     console.log('find it wow')
+// }
+// else {
+//     console.log('pailam na dada');
+// }
+
+// // how to use start with 
+// console.log(lirics.startsWith('tumi'));
+// console.log(lirics.endsWith('kala'));
+
+// split method 
+const lyrics = 'tumi bondhu kala pakhi ami jeno ki , boshonto kale tomay bolte parini. shada shada kala kala rong jomeche shada kala'
+// const parts = lyrics.split('');
+// const parts = lyrics.split(' ');
+// const parts = lyrics.split('.');
+// console.log(parts);
+const partial = lyrics.slice(5, 8);
+console.log(partial);
+
+const partial2 = lyrics.substring(5, 8);
+console.log(partial2);
